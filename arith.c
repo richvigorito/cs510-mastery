0475c5c7d7834cbbd68b3bf97e7fe7d7c2fafd8a
Rich Vigorito <vigor@pdx.edu>
2013-07-05 16:49:35 -0700

    fixes to arith fuctions to homework compliant

--- arith.c
+++ arith.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-int div(int a, int b){ //div(a,b) = a/b
+int modulo(int a, int b){ 
+//modulo(a,b) = a/b, ie a = divident, b = divisor
 
-	int quotient = 0 ;
+	if (0 == b) {
+		printf("Zero Divide\n");
+		return 0;
+	}	
+	else if (a < 0 && b < 0)
+		return -modulo(-a,-b);
+	else if (a < 0)
+		return -modulo(-a,b);
+	else if (b < 0)
+		return modulo(a,-b);
+	else if (a == b)
+		return 0;
+	else if (1 == b)
+		return 0;
+	else if (a < b)
+		return a;
+	else 
+		return modulo(sub(a,b),b);
+}
+
+
+int div(int a, int b){ 
+//div(a,b) = a/b, ie a = divident, b = divisor
+
+	int q = 0 ; // quotient
 
 	if (b == 1)
 		return a;
 
 	else if (0 == b) {
-		printf("\nraise divsion by 0 error\n");
-		return 0;	
+		printf("Zero Divide\n");
+		return 0;
 	}
 
 	else if (a < 0 && b < 0)
@@ -24,166 +49,162 @@ int div(int a, int b){ //div(a,b) = a/b
 	else if (a < b)
 		return 0;
 	else {
-		quotient = add(++quotient,div(sub(a,b),b));
-		return quotient;
+		q = add(++q,div(sub(a,b),b));
+		return q;
 	}
 }
 
-int _pow(int a, int b){
-// pow(a,b) ==  a ^ b
-	if (-1 == a)  
-		return ( b%2 ) ? -1 : 1;
+int expo(int a, int b){
+// expo(a,b) ==  a ^ b
+
+	if (-1 == a)
+		return (modulo(b,2)) ? -1 : 1;
 	else if (0 == b) 
 		return 1;
 	else if (1 == a)  
 		return 1;
 	else if (0 == a)  
 		return 0;
-	else if (0 > b){
-		printf("\nraise negative exponent error\n");
+	else if (0 > b)
 		return 0;
-	}else 
-		return mult(_pow(a,--b),a);
+	else 
+		return mult(expo(a,--b),a);
 }
 
 int mult(int a, int b){
+// mult(a,b) == a*b
 
-	if (1 == b)
+	if (1 == b)				// 1*b = b
 		return a;
-	else if (1 == a)
+	else if (1 == a)			// 1*a = a
 		return b;
-	else if (0 == b || 0 == a)
+	else if (0 == b || 0 == a) 		// 0 * a = 0 OR 0 * b = 0
 		return 0;
-	else if (a < 0 && b < 0)	
+	else if (a < 0 && b < 0)		// -a * -b  = a*b
 		return mult(-a,-b);
-	else if (a < 0)
+	else if (a < 0)				// -a*b = -(a*b)
 		return -mult(-a,b);
-	else if (b < 0)
+	else if (b < 0)				// a*-b = -(a*b)
 		return -mult(a,-b);
 	else 
-		return add(mult(a,--b),a); // 
+		return add(mult(a,--b),a);     // 
 
 }
 
-
 int sub(int a, int b){ 
+// sub(a,b) == a-b
 	return add(a,-b); // a - b == a + - b
 }
 
 int add(int a, int b){
+// add(a,b) == a+b
 	if (0 == b)
 		return a;
-	else if ( b < 0 ) // b is less than 0, work to get b to base case via increment
+	else if ( b < 0 ) 			// b is less than 0, work to get b to base case via increment
 		return add(--a,++b); 
-	else 		  // b is more than 0, work to get b to base case via decrement
+	else 		  			// b is more than 0, work to get b to base case via decrement
 		return add(++a,--b);
 
 }
 
-void test_pow(int lower,int upper){
+void test_expo(int lower,int upper){
 	int a,b,c;	
-	int failed = 0;
+	printf("Power: %d..%d ** %d..%d\n", lower,upper,lower,upper);
  	for (a = lower; a <= upper; a++){
  		for (b = lower; b <= upper; b++){
-			c = _pow(a,b);
-			if (c != pow(a,b)){
-				printf("%d ^ %d = %d \n", a,b,c);
-				printf("%f != %d \n", pow(a,b),c);
-				failed = 1;	
-			}
+			c = expo(a,b);
+			printf("%d ** %d = %d \n", a,b,c);
+			if(b<0)
+				continue;
+			if (c != pow(a,b))
+				printf("ERROR: %f != %d \n", pow(a,b),c);
 		}
 	}
-	if (failed){
-		printf("\ntest pow function failed\n");
-	} else {
-		printf("\ntest pow function passed\n");
-	}
+	printf("\n");
 }
 
-void test_div(int lower,int upper){
+void test_modulo(int lower,int upper){
 	int a,b,c;	
-	int failed = 0;
+	printf("Modulus: %d..%d %% %d..%d\n", lower,upper,lower,upper);
  	for (a = lower; a <= upper; a++){
  		for (b = lower; b <= upper; b++){
 			if(0 == b)
 				continue;
-			c = div(a,b);
-			if (c != ceil(a / b)){
-				printf("%d / %d = %d \n", a,b,c);
-				failed = 1;	
-			}
+			c = modulo(a,b);
+			printf("%d %% %d != %d \n", a,b,c);
+			if (c != (a % b))
+				printf("ERROR %d != %d \n", c,a%b);
 		}
 	}
-	if (failed){
-		printf("\ntest div function failed\n");
-	} else {
-		printf("\ntest div function passed\n");
+	printf("\n");
+}
+
+void test_div(int lower,int upper){
+	int a,b,c;	
+	printf("Division: %d..%d / %d..%d\n", lower,upper,lower,upper);
+ 	for (a = lower; a <= upper; a++){
+ 		for (b = lower; b <= upper; b++){
+			c = div(a,b);
+			printf("%d / %d = %d \n", a,b,c);
+			if(0 == b)
+				continue;
+			if (c != ceil(a / b))
+				printf("ERROR %d / %d != %f \n", a,b,ceil(a / b));
+		}
 	}
+	printf("\n");
 }
 
 void test_mult(int lower,int upper){
 	int a,b,c;	
-	int failed = 0;
+	printf("Multiplication: %d..%d * %d..%d\n", lower,upper,lower,upper);
  	for (a = lower; a <= upper; a++){
  		for (b = lower; b <= upper; b++){
 			c = mult(a,b);
-			if (c != (a*b)){
-				printf("%d * %d = %d \n", a,b,c);
-				failed = 1;	
-			}
+			printf("%d * %d = %d \n", a,b,c);
+			if (c != (a*b))
+				printf("ERROR %d * %d != %d \n", a,b,a*b);
 		}
 	}
-	if (failed){
-		printf("\ntest mult function failed\n");
-	} else {
-		printf("\ntest mult function passed\n");
-	}
+	printf("\n");
 }
 
 void test_sub(int lower,int upper){
 	int a,b,c;	
 	int failed = 0;
+	printf("Subtraction: %d..%d - %d..%d\n", lower,upper,lower,upper);
  	for (a = lower; a <= upper; a++){
  		for (b = lower; b <= upper; b++){
 			c = sub(a,b);
-			if (c != (a-b)){
-				printf("%d - %d = %d \n", a,b,c);
-				failed = 1;	
-			}
+			printf("%d - %d = %d \n", a,b,c);
+			if (c != (a-b))
+				printf("%d - %d != %d \n", a,b,a-b);
 		}
 	}
-	if (failed){
-		printf("\ntest sub function failed\n");
-	} else {
-		printf("\ntest sub function passed\n");
-	}
+	printf("\n");
 }
 
 void test_add(int lower,int upper){
 	int a,b,c;	
-	int failed = 0;
+	printf("Addition: %d..%d + %d..%d \n", lower,upper,lower,upper);
  	for (a = lower; a <= upper; a++){
  		for (b = lower; b <= upper; b++){
 			c = add(a,b);
-			if (c != (a+b)){
-				printf("%d + %d = %d \n", a,b,c);
-				failed = 1;	
-			}
+			printf("%d + %d = %d \n", a,b,c);
+			if (c != (a+b))
+				printf("%d + %d != %d \n", a,b,a+b);
 		}
 	}
-	if (failed){
-		printf("\ntest add function failed\n");
-	} else {
-		printf("\ntest add function passed\n");
-	}
+	printf("\n");
 }
 
 int main()
 {
-	test_add(-100,100);
-	test_sub(-100,100);
-	test_mult(-100,100);
-	test_div(-100,100);
-	test_pow(-1,5);
-
+	test_add(-6,5);
+	test_sub(-6,5);
+	test_mult(-6,5);
+	test_div(-6,5);
+	test_expo(-6,5);
+//	test_modulo(-6,5);
+	return 0;
 }
